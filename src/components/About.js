3c5fcdafd54c519eab71bf26d2af23fd7c5daa64
FarzanaTN <nishatfarzana14@gmail.com>
2025-06-21 18:48:50 +0600

    update

--- src/components/About.js
+++ src/components/About.js
@@ -3,7 +3,7 @@ import './About.css';
 
 const About = () => (
   <section id="about" className="fade-in">
-    <img src="../profile.jpg" alt="Farzana Tasnim" className="profile-img" />
+    <img src="../../profile.jpg" alt="Farzana Tasnim" className="profile-img" />
     <h2>About Me</h2>
     <p>Assalamualaikum !I’m a passionate developer who loves building beautiful interfaces and smooth user experiences. I work with React, Flutter, and more.</p>
   </section>
