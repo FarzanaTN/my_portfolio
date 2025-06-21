90a66f4a785ebca441938d4c5a0dc699000a92dc
FarzanaTN <nishatfarzana14@gmail.com>
2025-06-21 18:45:47 +0600

    update

--- src/App.js
+++ src/App.js
@@ -5,10 +5,41 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import Contact from './components/Contact';
 
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+const particlesInit = async (main) => {
+  await loadFull(main);
+};
+
+
 function App() {
 
   return (
+
     <div className="App">
+      <Particles
+        id="tsparticles"
+        init={particlesInit}
+        options={{
+          background: { color: { value: "transparent" } },
+          fpsLimit: 60,
+          interactivity: {
+            events: { onHover: { enable: true, mode: "repulse" }, resize: true },
+            modes: { repulse: { distance: 100, duration: 0.4 } }
+          },
+          particles: {
+            color: { value: "#007acc" },
+            links: { enable: true, color: "#007acc", distance: 150, opacity: 0.5, width: 1 },
+            move: { enable: true, speed: 1 },
+            number: { value: 50 },
+            opacity: { value: 0.5 },
+            shape: { type: "circle" },
+            size: { value: { min: 1, max: 5 } }
+          }
+        }}
+      />
+
       <header>
         <h1>Farzana Tasnim</h1>
         <nav>
