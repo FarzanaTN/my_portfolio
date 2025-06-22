6ee56e996bb1216b6fa7ff71751013cd89ba03b4
FarzanaTN <nishatfarzana14@gmail.com>
2025-06-22 20:57:28 +0600

    project video

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -6,32 +6,32 @@ const Projects = () => (
     <div className="project">
       <h3>Cosmo Shooter Game</h3>
       <p>A shooting game using SDL2 for c language.</p>
-      <a href="https://github.com/yourusername/weather-app">GitHub</a> |
-      <a href="https://weatherapp-demo.netlify.app">Live Demo</a>
+      <a href="https://github.com/FarzanaTN/cosmp_shooter">GitHub</a> |
+      <a href="https://www.youtube.com/watch?v=PESr5fFNJCo">Live Demo</a>
     </div>
     {/* Add more projects here */}
     <div className="project">
       <h3>Hotel Management System</h3>
       <p>A java project using java swing.</p>
-      <a href="https://github.com/yourusername/weather-app">GitHub</a> |
-      <a href="https://weatherapp-demo.netlify.app">Live Demo</a>
+      <a href="https://github.com/FarzanaTN/HotelManagementSystem">GitHub</a> |
+      <a href="https://drive.google.com/file/d/1fWMEJN6H49mIlJFoVi9YstGGuwMEPjpT/view">Demo</a>
     </div>
     <div className="project">
       <h3>Learnloop</h3>
       <p>An android project using flutter.</p>
-      <a href="https://github.com/yourusername/weather-app">GitHub</a> |
-      <a href="https://weatherapp-demo.netlify.app">Live Demo</a>
+      <a href="https://github.com/FarzanaTN/learnloop">GitHub</a> |
+      <a href="https://drive.google.com/file/d/1YydCDekDBhEHkHPqtTMFlpGUagg4nywZ/view">Demo</a>
     </div>
     <div className="project">
       <h3>Hotel Management System (DBMS)</h3>
       <p>A database project using mysql.</p>
-      <a href="https://github.com/yourusername/weather-app">GitHub</a> |
-      <a href="https://weatherapp-demo.netlify.app">Live Demo</a>
+      <a href="https://github.com/FarzanaTN/Hms_Dbms">GitHub</a> |
+      {/* <a href="https://weatherapp-demo.netlify.app">Live Demo</a> */}
     </div>
     <div className="project">
       <h3>MedWise</h3>
       <p>A software project using next-js.</p>
-      <a href="https://github.com/yourusername/weather-app">GitHub</a> |
+      <a href="https://github.com/FarzanaTN/MedWise">GitHub</a> |
       <a href="https://weatherapp-demo.netlify.app">Live Demo</a>
     </div>
   </section>
