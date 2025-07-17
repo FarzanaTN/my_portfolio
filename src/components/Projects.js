cdb2d18fc5b5ad0a6412268e597a6ecb4d779532
FarzanaTN <nishatfarzana14@gmail.com>
2025-07-17 19:33:18 +0600

    filefusion

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -38,7 +38,7 @@ const Projects = () => (
       <h3>FileFusion</h3>
       <p>A netwokring project to convert files.</p>
       <a href="https://github.com/FarzanaTN/FileFusion">GitHub</a> |
-      <a href="https://weatherapp-demo.netlify.app">Demo</a>
+      <a href="https://drive.google.com/drive/u/0/home">Demo</a>
     </div>
   </section>
 );
